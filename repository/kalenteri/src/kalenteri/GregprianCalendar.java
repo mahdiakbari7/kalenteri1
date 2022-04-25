6bda79caa01a6d72a5acc3e3ef123b011feafb21
mahdiakbari7 <104360724+mahdiakbari7@users.noreply.github.com>
2022-04-25 16:57:42 +0300

    Add files via upload

---
+++ repository/kalenteri/src/kalenteri/GregprianCalendar.java
@@ -0,0 +1,55 @@
+package kalenteri;
+
+import java.util.Calendar;
+
+public class GregprianCalendar extends Calendar {
+
+	@Override
+	protected void computeTime() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void computeFields() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void add(int field, int amount) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void roll(int field, boolean up) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public int getMinimum(int field) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getMaximum(int field) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getGreatestMinimum(int field) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getLeastMaximum(int field) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
